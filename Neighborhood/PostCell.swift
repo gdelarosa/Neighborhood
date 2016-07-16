18380f2ee54fd6226b86734b664ca68745f927b6
Gina De La Rosa <g26delarosa@gmail.com>
2016-07-16 13:56:37 -0700

    added files

---
+++ Neighborhood/PostCell.swift
@@ -0,0 +1,30 @@
+//
+//  PostCell.swift
+//  Neighborhood
+//
+//  Created by Gina De La Rosa on 11/26/15.
+//  Copyright © 2015 Gina De La Rosa. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var descLbl: UILabel! 
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        postImg.layer.cornerRadius = postImg.frame.size.width / 2
+        postImg.clipsToBounds = true
+    }
+    
+    func configureCell(post: Post) {
+        titleLbl.text = post.title
+        descLbl.text = post.postDesc
+        postImg.image = DataService.instance.imageForPath(post.imagePath)
+    }
+    
+}
